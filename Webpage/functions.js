6fb5dc20b81e6bb145d7b852c533402717417c38
FCBFAN05 <melvin_troesch@sluz.ch>
2022-06-05 16:43:04 +0200

    Programmierung Fortschritt 1 Caesar und HTML-Simple-DES

--- Webpage/functions.js
+++ Webpage/functions.js
@@ -2,7 +2,7 @@
 
 /* Funktionen zur Verschlüsselung */
 function copy(){
-    document.getElementById("chiffrentext").value = document.getElementById("klartext").value
+    document.getElementById("L-Half_S-DES-u").value = document.getElementById("L-Half_S-DES").value
 }
 
 function encode(){
@@ -12,4 +12,19 @@ function encode(){
     let secondPart = text.substring(text.length/2, text.length)
     document.getElementById("chiffrentext").value = secondPart.concat(firstPart)
 
+}
+
+function caesar(){
+    let clearText = document.getElementById("klartext_caesar").value
+    let movement = parseInt(document.getElementById("verschiebung_caesar").value)
+    let cipherText = ""
+
+    let ascii = [clearText.length]
+    for (let i=0; i<clearText.length; i++) {
+        ascii[i] = clearText.charCodeAt(i)
+    }
+    for (let i=0; i<ascii.length; i++) {
+        cipherText += String.fromCharCode(ascii[i]+movement)
+    }
+    document.getElementById("chiffrentext_caesar").value = cipherText
 }
