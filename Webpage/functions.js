932f5bc189d6fdf95907d1e32392fa40222f3162
FCBFAN05 <melvin_troesch@sluz.ch>
2022-07-04 23:37:48 +0200

    convertToBits mit Paritätsbit und Abstand nach 8 Bits

--- Webpage/functions.js
+++ Webpage/functions.js
@@ -19,6 +19,7 @@ function encode(){
 
  */
 
+
 function caesar(){
     let clearText = document.getElementById("klartext_caesar_textarea").value // Namen anschreiben --> WElches Element die Variable vertritt
     let movement = parseInt(document.getElementById("verschiebung_caesar_textarea").value)
@@ -38,10 +39,25 @@ function caesar(){
 
 
 function convertToBits() {
+    document.getElementById("bits_S-DES_textarea").value = ""
     let text = document.getElementById("klartext_S-DES_textarea").value.toUpperCase()
-    for (let i = 0; i < text.length; i++){
-        let bitcode = parseInt(text.charCodeAt(i).toString(2)) // Funktionen/ Methoden von Grund auf machen
-        document.getElementById("bits_S-DES_textarea").value += bitcode
+    let parity = document.getElementById("evenoroddparityinput_textarea").value
+    let bitcode = [text.length]
+    let ckecksum = 0
+
+
+    for (let i = 0; i < text.length; i++) {
+        ckecksum += parseInt(text.substr(i,text.length))
+        if ((ckecksum % 2) === 0 && parity === "e") {
+            bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0") // Funktionen/ Methoden von Grund auf machen
+        } else if ((ckecksum % 2) !== 0 && parity === "e") {
+            bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
+        } else if ((ckecksum % 2) === 0 && parity === "o") {
+            bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
+        } else {
+            bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0")
+        }
+        document.getElementById("bits_S-DES_textarea").value += (bitcode[i] + " ")
     }
 }
     //console.log(parseInt(document.getElementById("klartext_S-DES").value.toUpperCase().charCodeAt(0).toString(),2).toString())
