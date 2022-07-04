670dc2935b67307140e5de05aea592ffb9b5592c
FCBFAN05 <melvin_troesch@sluz.ch>
2022-07-04 16:37:31 +0200

    Caesar-Chiffre funktioniert und komprimiert convertToBits funktioniert mit 7-Bit Ausgabe ohne Paritätsbit elementId überall passend zur Funktion umbenannt

--- Webpage/functions.js
+++ Webpage/functions.js
@@ -1,8 +1,11 @@
 /* Funktionen für die Webseite */
 
 /* Funktionen zur Verschlüsselung */
+
+/* Beispiel-Funktionen:
+
 function copy(){
-    document.getElementById("chiffrentext").value = document.getElementById("klartext").value
+    document.getElementById("L-Half_S-DES-u").value = document.getElementById("L-Half_S-DES").value
 }
 
 function encode(){
@@ -13,3 +16,47 @@ function encode(){
     document.getElementById("chiffrentext").value = secondPart.concat(firstPart)
 
 }
+
+ */
+
+function caesar(){
+    let clearText = document.getElementById("klartext_caesar_textarea").value // Namen anschreiben --> WElches Element die Variable vertritt
+    let movement = parseInt(document.getElementById("verschiebung_caesar_textarea").value)
+    let cipherText = ""
+    let ascii = [clearText.length]
+
+    for (let i=0; i<clearText.length; i++) {
+        ascii[i] = clearText.toUpperCase().charCodeAt(i)
+        let newChar = ((ascii[i]+movement-65)%26)+65
+        console.log(newChar)
+        cipherText += String.fromCharCode(newChar)
+        console.log(cipherText)
+    }
+    document.getElementById("chiffrentext_caesar_textarea").value = cipherText
+}
+
+
+
+function convertToBits() {
+    let text = document.getElementById("klartext_S-DES_textarea").value.toUpperCase()
+    for (let i = 0; i < text.length; i++){
+        let bitcode = parseInt(text.charCodeAt(i).toString(2)) // Funktionen/ Methoden von Grund auf machen
+        document.getElementById("bits_S-DES_textarea").value += bitcode
+    }
+}
+    //console.log(parseInt(document.getElementById("klartext_S-DES").value.toUpperCase().charCodeAt(0).toString(),2).toString())
+    //document.getElementById("bits_S-DES").value = parseInt(document.getElementById("klartext_S-DES").value,2).toString()
+
+
+function runAll() {
+
+}
+
+/* function run(checkbox[true]) {
+
+} // Mit Checkboxes anwählen was angezeigt/ ausgeführt wird und was nicht
+
+
+ */
+
+// Mails verschicken für Kommunikation miteinander
