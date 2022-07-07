9e8ab0e25bf6a3aec2a67f4c46455f234806d018
FCBFAN05 <melvin_troesch@sluz.ch>
2022-07-07 13:06:04 +0200

    Permutation gemacht → Ausgabe funktioniert noch nicht navbar mit css gemacht

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -19,6 +19,14 @@ function encode(){
 
  */
 
+function fakultaet(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
 
 function caesar(){
     let clearText = document.getElementById("klartext_caesar_textarea").value // Namen anschreiben --> WElches Element die Variable vertritt
@@ -63,44 +71,54 @@ function convertToBits() {
 function initialpermutation() {
     let text = document.getElementById("klartext_S-DES_textarea").value.toUpperCase()
     let parity = document.getElementById("evenoroddparityinput_textarea").value
-    let bitcodeOutOfText = [text.length][8]
+    let bitcodeOutOfText = [text.length]
+    let bitcodeSplitArray = [8]
+    let resultArray = [fakultaet(8)]
     let ckecksum = 0
 
-    for (let i = 0; i < text.length; i++) {
-        for (let j = 0; j < 8; j++) {
-            ckecksum += parseInt(text.substr(i, text.length))
-            if ((ckecksum % 2) === 0 && parity === "e") {
-                bitcodeOutOfText[i][j] = parseInt(text.charCodeAt(i).toString(2) + "0") // Funktionen/ Methoden von Grund auf machen
-            } else if ((ckecksum % 2) !== 0 && parity === "e") {
-                bitcodeOutOfText[i][j] = parseInt(text.charCodeAt(i).toString(2) + "1")
-            } else if ((ckecksum % 2) === 0 && parity === "o") {
-                bitcodeOutOfText[i][j] = parseInt(text.charCodeAt(i).toString(2) + "1")
+    const permutator = (inputArr) => {
+        let result = [];
+
+        const permute = (arr, m = []) => {
+            if (arr.length === 0) {
+                result.push(m)
             } else {
-                bitcodeOutOfText[i][j] = parseInt(text.charCodeAt(i).toString(2) + "0")
-            }
-        }
-
-        const permutator = (inputArr) => {
-            let result = []
-
-            const permute = (arr, m = []) => {
-                if (arr.length === 0) {
-                    result.push(m)
-                } else {
-                    for (let i = 0; i < arr.length; i++) {
-                        let curr = arr.slice()
-                        let next = curr.splice(i, 1)
-                        permute(curr.slice(), m.concat(next))
-                    }
+                for (let i = 0; i < arr.length; i++) {
+                    let curr = arr.slice();
+                    let next = curr.splice(i, 1);
+                    permute(curr.slice(), m.concat(next))
                 }
             }
-            permute(inputArr)
-            return result
         }
-        permutator(bitcodeOutOfText[i])
+
+        permute(inputArr)
+
+        return result;
+    }
+
+    for (let i = 0; i < text.length; i++) {
+            ckecksum += parseInt(text.substr(i, text.length))
+            if ((ckecksum % 2) === 0 && parity === "e") {
+                bitcodeOutOfText[i] = parseInt(text.charCodeAt(i).toString(2) + "0") // Funktionen/ Methoden von Grund auf machen
+            } else if ((ckecksum % 2) !== 0 && parity === "e") {
+                bitcodeOutOfText[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
+            } else if ((ckecksum % 2) === 0 && parity === "o") {
+                bitcodeOutOfText[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
+            } else {
+                bitcodeOutOfText[i] = parseInt(text.charCodeAt(i).toString(2) + "0")
+            }
+            for (let j = 0; j < 8; j++) {
+                let bitcodeSplit = bitcodeOutOfText[i].toString().substring(j,j+1)
+                console.log(bitcodeSplit)
+                bitcodeSplitArray[j] = bitcodeSplit
+                parseInt(bitcodeSplit)
+            }
+            for (let k = 0; k < fakultaet(8); k++) {
+                resultArray[k] = permutator(bitcodeSplitArray)
+            }
+            document.getElementById("initialpermutation_S-DES_textarea").value += resultArray[Math.random()*fakultaet(8)]
     }
 }
-document.getElementById("initial permutation_S-DES_textarea").value = permutator(bitcodeOutOfText[Math.random()*text.length])
 
 function runAll() {
 
