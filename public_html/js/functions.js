9e3b65fd6d24039e19281b033f02e2def76f28c7
FCBFAN05 <melvin_troesch@sluz.ch>
2022-07-25 20:32:28 +0200

    Problem noch nicht gelöst

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -68,18 +68,18 @@ function convertToBits() {
     document.getElementById("bits_S-DES_textarea").value = ""
     let text = document.getElementById("klartext_S-DES_textarea").value.toUpperCase()
     let parity = document.getElementById("evenoroddparityinput_textarea").value
-    let bitcode = [text.length]
-    let ckecksum = 0
+    let bitcode = []
+    let checksum = 0
 
     for (let i = 0; i < text.length; i++) {
-        ckecksum += parseInt(text.substr(i,text.length))
-        if ((ckecksum % 2) === 0 && parity === "e") {
+        checksum += parseInt(text.substr(i,text.length))
+        if ((checksum % 2) === 0 && parity === "e") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0") // Funktionen/ Methoden von Grund auf machen
-        } else if ((ckecksum % 2) !== 0 && parity === "e") {
+        } else if ((checksum % 2) !== 0 && parity === "e") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
-        } else if ((ckecksum % 2) === 0 && parity === "o") {
+        } else if ((checksum % 2) === 0 && parity === "o") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
-        } else if ((ckecksum % 2) !== 0 && parity === "o"){
+        } else if ((checksum % 2) !== 0 && parity === "o"){
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0")
         } else {
             document.getElementById("bits_S-DES_textarea").value = "parity "
@@ -90,34 +90,33 @@ function convertToBits() {
     //console.log(parseInt(document.getElementById("klartext_S-DES").value.toUpperCase().charCodeAt(0).toString(),2).toString())
     //document.getElementById("bits_S-DES").value = parseInt(document.getElementById("klartext_S-DES").value,2).toString()
 
-
-
 function initialpermutation() {
     let text = document.getElementById("klartext_S-DES_textarea").value.toUpperCase()
     let parity = document.getElementById("evenoroddparityinput_textarea").value
-    let bitcode = [text.length]
-    let ckecksum = 0
-    let bits = [text.length]
+    let bitcode = []
+    let checksum = 0
+    let bits = []
 
     for (let i = 0; i < text.length; i++) {
-        ckecksum += parseInt(text.substr(i,text.length))
-        if ((ckecksum % 2) === 0 && parity === "e") {
+        checksum += parseInt(text.substr(i,text.length))
+        if ((checksum % 2) === 0 && parity === "e") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0") // Funktionen/ Methoden von Grund auf machen
-        } else if ((ckecksum % 2) !== 0 && parity === "e") {
+        } else if ((checksum % 2) !== 0 && parity === "e") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
-        } else if ((ckecksum % 2) === 0 && parity === "o") {
+        } else if ((checksum % 2) === 0 && parity === "o") {
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "1")
-        } else if ((ckecksum % 2) !== 0 && parity === "o"){
+        } else if ((checksum % 2) !== 0 && parity === "o"){
             bitcode[i] = parseInt(text.charCodeAt(i).toString(2) + "0")
         } else {
             document.getElementById("bits_S-DES_textarea").value = "parity "
         }
-        for (let j=0; j<8; j++) {
-            bits[i] = [bitcode[i].toString().length]
-            bits[i][j] = bitcode[i].toString().substring(j,j+1)
-            console.log(bits[i][j])
+        console.log(bitcode)
+        for (let j = 0; j < 8; j++) {
+            bits[i] = [bitcode[i].toString().substring(j, j+1)]
+            console.log(bits[i])
         }
-        document.getElementById("initialpermutation_S-DES_textarea").value += (bits[i][1].concat(bits[i][5]).concat(bits[i][2]).concat(bits[i][0]).concat(bits[i][3]).concat(bits[i][7]).concat(bits[i][4]).concat(bits[i][6]) + " ")
+        console.log(bits[i][1])
+        document.getElementById("initialpermutation_S-DES_textarea").value += (bits[i][1].toString().concat(bits[i][5]).concat(bits[i][2]).concat(bits[i][0]).concat(bits[i][3]).concat(bits[i][7]).concat(bits[i][4]).concat(bits[i][6]) + " ")
     }
 }
 
